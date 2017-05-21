bf9d14a680a5325abb009ad49b956f820362d412
lbrgulja1 <lbrgulja1@etf.unsa.ba>
2017-05-21 17:42:56 +0200

    Push paymentmanager, paymentmethod, paymentmethoddbcontext i parkingdbcontext

---
+++ Projekat/SPARK/SPARK/Model/PaymentManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPARK.Model
+{
+    public class PaymentManager
+    {
+        private List<PaymentMethod> paymentMethods;
+
+        public List<PaymentMethod> PaymentMethods
+        {
+            get
+            {
+                return paymentMethods;
+            }
+
+            set
+            {
+                paymentMethods = value;
+            }
+        }
+    }
+}
---
+++ Projekat/SPARK/SPARK/Model/PaymentMethod.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SPARK.Model
+{
+    public class PaymentMethod
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int id;
+        private string name;
+        private string description;
+        private List<Tuple<int, double>> packages;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public List<Tuple<int, double>> Packages
+        {
+            get
+            {
+                return packages;
+            }
+
+            set
+            {
+                packages = value;
+            }
+        }
+    }
+}
