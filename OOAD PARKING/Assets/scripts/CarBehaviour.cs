57e0b7b743823ae1f0978900c27c1389b9a3bf7c
rakejam <kjamakovic1@etf.unsa.ba>
2017-06-05 01:25:18 +0200

    OOAD PARKING igra

---
+++ OOAD PARKING/Assets/scripts/CarBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviour : MonoBehaviour {
+
+	public float power = 3;
+	public float maxspeed = 5;
+	public float turnpower = 2;
+	public float friction = 3;
+	public Vector2 curspeed ;
+	Rigidbody2D rigidbody2D;
+	UnityEngine.Vector3 spos;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+		spos = transform.position;
+	}
+
+
+	void FixedUpdate()
+	{
+		curspeed = new Vector2(rigidbody2D.velocity.x,    rigidbody2D.velocity.y);
+
+		if (curspeed.magnitude > maxspeed)
+		{
+			curspeed = curspeed.normalized;
+			curspeed *= maxspeed;
+		}
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			rigidbody2D.AddForce(transform.up * power);
+			rigidbody2D.drag = friction;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			rigidbody2D.AddForce(-(transform.up) * (power/2));
+			rigidbody2D.drag = friction;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * turnpower);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.forward * -turnpower);
+		}
+	}
+	void OnCollisionEnter(Collision col){
+		transform.position = spos;
+	}
+
+}
